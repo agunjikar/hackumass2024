9bfd3ca6ce29df3dd3cb75ca71a32d64e1e75b25
Anshul Saha <anshulsaha15@gmail.com>
2024-11-09 22:20:42 -0500

    discovery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Register from './components/Register';
+import Discovery from "./components/Discovery";
 import './global.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="register" element={<Register />} />
+          <Route path="discovery" element={<Discovery />} />
         </Route>
       </Routes>
     </BrowserRouter>
