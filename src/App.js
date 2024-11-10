94f000835c00ce47ba2f67d1a7cfb583fc9a8255
agunjikar <agunjikar@umass.edu>
2024-11-09 22:26:02 -0500

    added changes to the bids

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Register from './components/Register';
+import OngoingBid from "./components/OngoingSelling";
 import './global.css'
+import CompletedBid from "./components/CompletedBuying";
+import CompletedSelling from "./components/CompletedSelling";
+import CompletedBuying from "./components/CompletedBuying";
+import OngoingSelling from "./components/OngoingSelling";
 
 function App() {
   return (
@@ -10,6 +15,9 @@ function App() {
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="register" element={<Register />} />
+          <Route path="ongoingselling" element={<OngoingSelling />} />
+          <Route path="completedbuying" element={<CompletedBuying />} />
+          <Route path="completedselling" element={<CompletedSelling />} />
         </Route>
       </Routes>
     </BrowserRouter>
